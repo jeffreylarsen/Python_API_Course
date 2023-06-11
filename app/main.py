b9333373822fa38d1a079bff42ab4c62237d2f1d
Jeffrey Larsen <jeffreydeanlarsen@gmail.com>
2023-06-11 13:18:11 -0600

    Trying to add shows

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
-from .routers import auth, posts, users, votes, websocket, threeDeffect
+from .routers import auth, posts, users, votes, shows, websocket, threeDeffect
 
 # models.Base.metadata.create_all(bind=engine)
 
@@ -24,6 +24,7 @@ app.include_router(auth.router)
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(votes.router)
+app.include_router(shows.router)
 app.include_router(websocket.router)
 app.include_router(threeDeffect.router)
 
