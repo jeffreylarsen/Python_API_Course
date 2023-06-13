ca4aa68778d399221397179eb90118721756c6b3
Jeffrey Larsen <jeffreydeanlarsen@gmail.com>
2023-06-13 16:17:56 -0600

    rework of alembic revisions for moving towards rundowns

--- app/routers/shows.py
+++ app/routers/shows.py
@@ -12,39 +12,42 @@ router = APIRouter(
 
 @router.get('/')
 def get_all_shows(
-        # db: Session = Depends(get_db),
+        db: Session = Depends(get_db)
         # current_user: int = Depends(Oauth2.get_current_user)
     ):
 
-    # posts = db.query(models.Show).all()
+    posts = db.query(models.Show).all()
     # print(posts)
 
     #return an array of shows with air and end dates from the database 
-    return [
-        {
-            "id": 1,
-            "show_name": "The Office",
-            "show_air_time":"2023-05-24T15:53:39.169735+00:00",
-            "show_end_time":"2023-05-24T15:53:39.169735+00:00"
-        },
-        {
-            "id": 2,
-            "show_name": "Parks and Rec",
-            "show_air_time":"2023-05-24T15:53:39.169735+00:00",
-            "show_end_time":"2023-05-24T15:53:39.169735+00:00"
-        },
-        {
-            "id": 3,
-            "show_name": "The Good Place",
-            "show_air_time":"2023-05-24T15:53:39.169735+00:00",
-            "show_end_time":"2023-05-24T15:53:39.169735+00:00"
-        },{
-            "id": 4,
-            "show_name": "Brooklyn 99",
-            "show_air_time":"2023-05-24T15:53:39.169735+00:00",
-            "show_end_time":"2023-05-24T15:53:39.169735+00:00"
-        }
-    ]
+    
+    return posts
+    
+    # return [
+    #     {
+    #         "id": 1,
+    #         "show_name": "The Office",
+    #         "show_air_time":"2023-05-24T15:53:39.169735+00:00",
+    #         "show_end_time":"2023-05-24T15:53:39.169735+00:00"
+    #     },
+    #     {
+    #         "id": 2,
+    #         "show_name": "Parks and Rec",
+    #         "show_air_time":"2023-05-24T15:53:39.169735+00:00",
+    #         "show_end_time":"2023-05-24T15:53:39.169735+00:00"
+    #     },
+    #     {
+    #         "id": 3,
+    #         "show_name": "The Good Place",
+    #         "show_air_time":"2023-05-24T15:53:39.169735+00:00",
+    #         "show_end_time":"2023-05-24T15:53:39.169735+00:00"
+    #     },{
+    #         "id": 4,
+    #         "show_name": "Brooklyn 99",
+    #         "show_air_time":"2023-05-24T15:53:39.169735+00:00",
+    #         "show_end_time":"2023-05-24T15:53:39.169735+00:00"
+    #     }
+    # ]
 
 
 
