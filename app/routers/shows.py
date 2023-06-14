068a7aa57c425939540244a2c93bb177a66dfcc0
Jeffrey Larsen <jeffreydeanlarsen@gmail.com>
2023-06-14 13:23:07 -0600

    query for all stories in a rundown

--- app/routers/shows.py
+++ app/routers/shows.py
@@ -58,7 +58,7 @@ def get_show_by_id(
         current_user: int = Depends(Oauth2.get_current_user)
     ):
 
-    show_stories = db.query(models.Story).filter(models.Story.show_id == show_id).all()
+    show_stories = db.query(models.Story).filter_by(models.Story.show_id == show_id)
     print(show_stories)
     
     return show_stories
