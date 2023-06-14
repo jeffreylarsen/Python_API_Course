06d8da11488fa8a0e785438a5b760ac21fffd342
Jeffrey Larsen <jeffreydeanlarsen@gmail.com>
2023-06-14 17:35:43 -0600

    query for all stories in a rundown

--- app/routers/stories.py
+++ app/routers/stories.py
@@ -10,14 +10,21 @@ router = APIRouter(
     tags=["Stories"]
 )
 
-@router.get('/')
+@router.get('/', response_model=List[schemas.Story])
 def get_all_stories(
         db: Session = Depends(get_db),
         current_user: int = Depends(Oauth2.get_current_user)
     ):
 
+    if db:
+        print("Well the database is getting accessed, here look...")
+        print(db)
+    else:
+        print("the database is fucked up")
+
+    print(db.query(models.Story))
+    print(db.query(models.Story).all())
     stories = db.query(models.Story).all()
-    # print(stories)
     
     return stories
 
