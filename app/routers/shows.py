e500631b63e000a54e80c21aa4d446aae798eab5
Jeffrey Larsen <jeffreydeanlarsen@gmail.com>
2023-06-14 13:29:20 -0600

    query for all stories in a rundown

--- app/routers/shows.py
+++ app/routers/shows.py
@@ -18,38 +18,8 @@ def get_all_shows(
 
     posts = db.query(models.Show).all()
     # print(posts)
-
-    #return an array of shows with air and end dates from the database 
     
     return posts
-    
-    # return [
-    #     {
-    #         "id": 1,
-    #         "show_name": "The Office",
-    #         "show_air_time":"2023-05-24T15:53:39.169735+00:00",
-    #         "show_end_time":"2023-05-24T15:53:39.169735+00:00"
-    #     },
-    #     {
-    #         "id": 2,
-    #         "show_name": "Parks and Rec",
-    #         "show_air_time":"2023-05-24T15:53:39.169735+00:00",
-    #         "show_end_time":"2023-05-24T15:53:39.169735+00:00"
-    #     },
-    #     {
-    #         "id": 3,
-    #         "show_name": "The Good Place",
-    #         "show_air_time":"2023-05-24T15:53:39.169735+00:00",
-    #         "show_end_time":"2023-05-24T15:53:39.169735+00:00"
-    #     },{
-    #         "id": 4,
-    #         "show_name": "Brooklyn 99",
-    #         "show_air_time":"2023-05-24T15:53:39.169735+00:00",
-    #         "show_end_time":"2023-05-24T15:53:39.169735+00:00"
-    #     }
-    # ]
-
-
 
 @router.get('/{show_id}', response_model=List[schemas.Story])
 def get_show_by_id(
@@ -58,7 +28,7 @@ def get_show_by_id(
         current_user: int = Depends(Oauth2.get_current_user)
     ):
 
-    show_stories = db.query(models.Story).filter_by(models.Story.show_id == show_id)
+    show_stories = db.query(models.Story).filter_by(show_id = show_id).all()
     print(show_stories)
     
     return show_stories
