8eb817165d265b74fc28f0c9acf1f49f9b726168
Jeffrey Larsen <jeffreydeanlarsen@gmail.com>
2023-06-14 17:43:42 -0600

    query for all stories in a rundown

--- app/routers/stories.py
+++ app/routers/stories.py
@@ -1,5 +1,5 @@
 from fastapi import Response, status, HTTPException, Depends, APIRouter
-from sqlalchemy import func
+from sqlalchemy import func, text
 from sqlalchemy.orm import Session
 from .. import models, schemas, Oauth2
 from typing import List, Optional
@@ -18,7 +18,7 @@ def get_all_stories(
 
     # print(db.query(models.Story))
     # print(db.query(models.Story).all())
-    stories = db.execute("SELECT * FROM stories")
+    stories = db.execute(text("SELECT * FROM stories"))
     print(stories)
     
     return stories
