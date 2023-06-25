88fa66b8ad4e913e4a9b530805e08b2020ade2bc
Jeffrey Larsen <jeffreydeanlarsen@gmail.com>
2023-06-25 01:28:38 -0600

    trying different things to make the create story call work

--- app/routers/stories.py
+++ app/routers/stories.py
@@ -49,12 +49,12 @@ def create_a_story(
     ):
 
     new_story = models.Story(
-        # created_by=current_user.username,
-        # last_modified_by=current_user.username,
-        # estimated_time=calculate_reading_time(story.script, 100),
+        created_by=current_user.username,
+        last_modified_by=current_user.username,
+        estimated_time=calculate_reading_time(story.script, 100),
         **story.dict()
     )
-    print(new_story)
+    print(**story.dict())
     db.add(new_story)
     db.commit()
     db.refresh(new_story)
