633bf5c72f59ea2ef8da908e4459eb81b30b888d
Jeffrey Larsen <jeffreydeanlarsen@gmail.com>
2023-06-29 20:57:59 -0600

    adding show model to story model

--- app/schemas.py
+++ app/schemas.py
@@ -109,7 +109,7 @@ class StoryModel(StoryBase):
     created_by: str
     last_modified_by: str
     estimated_time: str
-    show: ShowModel
+    show: List[StoryBase]
 
     class Config:
         orm_mode = True
