59ac8c88a6c6576498ce2d1fa3928dc92e7f588e
Jeffrey Larsen <jeffreydeanlarsen@gmail.com>
2023-07-01 16:45:34 -0600

    adding show model to story model

--- app/routers/stories.py
+++ app/routers/stories.py
@@ -23,7 +23,7 @@ def get_all_stories(
     return stories
 
 
-@router.get('/{id}', response_model=List[schemas.StoryModel])
+@router.get('/{id}', response_model=schemas.StoryModel)
 def get_a_story(
         id: int,
         db: Session = Depends(get_db),
