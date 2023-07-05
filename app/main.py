5163cd4eb2660e1e77b6066aa6dae4f7d282e933
Jeffrey Larsen <jeffreydeanlarsen@gmail.com>
2023-07-05 13:22:03 -0600

    Adding my Website to Root Endpoint

--- app/main.py
+++ app/main.py
@@ -37,7 +37,7 @@ from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="Portfolio_Website")
 
-@app.get("/index", response_class=HTMLResponse)
-def index(request: Request):
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
     context = {"request": request}
     return templates.TemplateResponse("index.html", context)
