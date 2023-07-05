47b4e6c5a5626bcb3df9ef9491841a1f9aa677bf
Jeffrey Larsen <jeffreydeanlarsen@gmail.com>
2023-07-05 13:39:47 -0600

    Adding my Website to Root Endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routers import auth, posts, users, votes, shows, stories, threeDeffect
-from .routers import portfolio
 from .routers.threeDeffect import get
 
 app = FastAPI()
