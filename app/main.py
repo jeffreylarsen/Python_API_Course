7cbe952dea7860f4edd79b913b5df8852d3471c2
Jeffrey Larsen <jeffreydeanlarsen@gmail.com>
2023-07-05 18:25:19 -0600

    Adding my Website to Root Endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import auth, posts, users, votes, shows, stories, threeDeffect
+from .routers import auth, posts, users, votes, shows, stories, threeDeffect, portfolio
 
 app = FastAPI()
+
 
 origins = ["*"]
 
@@ -21,10 +22,25 @@ app.include_router(votes.router)
 app.include_router(shows.router)
 app.include_router(stories.router)
 app.include_router(threeDeffect.router)
-# app.include_router(portfolio.router)
+app.include_router(portfolio.router)
 
-@app.get('/')
-def root():
-    threeDeffect.get()
-    # return {"hello":"Hello, Internet!!"}
+# @app.get('/')
+# def root():
+#     return {"hello":"Hello, Internet!!"}
+
+
+
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app.mount("/static", StaticFiles(directory="app/Portfolio_Website/images"), name="static")
+
+templates = Jinja2Templates(directory="app/Portfolio_Website")
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("index.html", context)
 
