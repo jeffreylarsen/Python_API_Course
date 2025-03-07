653c2f3e03c35c8897c2cccbd430def391d9cfba
jeffreylarsen <jeffreydeanlarsen@gmail.com>
2025-03-07 02:40:26 -0700

    refactor: update JavaScript event handling and enhance scroll reveal effects; improve HTML content and CSS styles

--- app/Portfolio_Website/images/index.js
+++ app/Portfolio_Website/images/index.js
@@ -2,11 +2,10 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-
-menuIcon.onClick = () => {
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-};
+}
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -26,11 +25,41 @@ window.onscroll = () => {
             });
         };
     });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
 
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
-
-let header = document.querySelector('header');
-
-header.classList.toggle('sticky', window.scrollY > 100);
+const typed = new Typed('.multi-text', {
+    strings: [
+        'Freelancer',
+        'Photographer',
+        'Content Creator',
+        'Software Developer',
+        'Video Editor',
+        'Influencer',
+        'Musician',
+        'Artist',
+    ],
+    typeSpeed: 60,
+    backSpeed: 50,
+    backDelay: 2000,
+    loop: true
+});
