7ac6a0a2d60a8b6692792d9037c6ff75b34f50a1
jeffreylarsen <jeffreydeanlarsen@gmail.com>
2025-03-10 04:52:49 -0600

    fix: update WebSocket URL to secure connection

--- app/routers/websocket.py
+++ app/routers/websocket.py
@@ -55,7 +55,7 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://python-api-course-jeffrey.herokuapp.com/websocket/ws");
+            var ws = new WebSocket("wss://jeffreylarsen.pro/websocket/ws");
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
